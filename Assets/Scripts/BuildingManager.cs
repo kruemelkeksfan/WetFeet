d99cb4907b464611ca3b7d5f0e19abd6689c1b18
Alfadas <alfadas@mail.goodevening-copr.de>
2018-08-11 16:16:25 +0200

    added building scripts, changed island asset

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    public void InsatciateBuilding(BuildableStruckture building)
+    {
+        GameObject currentBuilding = Instantiate(building.gameObject, transform.position, Quaternion.identity);
+        BuildableStruckture buildableStruckture = currentBuilding.GetComponent<BuildableStruckture>();
+
+    }
+}
