b7076c4ea3ad9f0c48b08c3cf0885f1cd046a9b8
Alfadas <alfadas@mail.goodevening-copr.de>
2018-08-11 15:16:18 +0200

    added editorGrid script

---
+++ Assets/Scripts/EditorGrid.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class EditorGrid : MonoBehaviour 
+{
+    void Update()
+    {
+        SnapToGrid();
+    }
+    void SnapToGrid()
+    {
+        transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y), Mathf.RoundToInt(transform.position.z));
+    }
+}
