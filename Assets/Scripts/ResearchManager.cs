bf4e7ab4f63407a71e81faecfb8255d82c00e91b
kruemelkeksfan <a_maximilian@web.de>
2018-08-12 04:04:35 +0200

    Added scene-change functionality and buttons, changed ResearchScreen.unity name and hooked ResearchManager.cs into maingame.unity

---
+++ Assets/Scripts/ResearchManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResearchManager : MonoBehaviour
+	{
+	// variable: List of accomplished researches
+
+	private void Awake()
+		{
+		DontDestroyOnLoad(this);
+		// initialize list
+		}
+
+	// adds the given research to the list of accomplished researches
+	public void addResearch(string researchname)
+		{
+
+		}
+
+	// returns, whether the given building is already researched
+	public bool checkPrequisite(GameObject buildingtype)
+		{
+		return true;
+		}
+	}
