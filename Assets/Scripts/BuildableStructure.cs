3dabb2cc10dfdd39f33d65140ec20b7d8e0f57c9
kruemelkeksfan <a_maximilian@web.de>
2018-08-13 15:52:58 +0200

    New Building System

--- Assets/Scripts/BuildableStructure.cs
+++ Assets/Scripts/BuildableStructure.cs
@@ -8,9 +8,8 @@ public class BuildableStructure : MonoBehaviour
 	public const string environmentTag = "Environment";
 	public const string structureTag = "Structure";
 
-	[SerializeField] int startHeight = 0;
+	[SerializeField] int startHeight = 100;
 
-	private int collidingEnvironment = 0;
 	private int collidingStructures = 0;
 	private bool hasPlaced = false;
 
@@ -21,12 +20,58 @@ public class BuildableStructure : MonoBehaviour
 			Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.y);
 			Vector3 mouseRPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 			transform.position = new Vector3(mouseRPosition.x, startHeight, mouseRPosition.z);
-			transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y), Mathf.RoundToInt(transform.position.z)); // snap to grid
-			GroundApproximation();
-			if(Input.GetMouseButtonDown(0) && checkTerrainConnection() && collidingStructures == 0) // building requirements
+
+			// Snap to grid
+			transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y), Mathf.RoundToInt(transform.position.z));
+
+			// Check terrain connection
+			RaycastHit hit;
+			Vector3 min = GetComponent<BoxCollider>().bounds.min;
+			Vector3 max = GetComponent<BoxCollider>().bounds.max;
+			Vector3 [] corners = new Vector3 [4];
+			corners [0] = new Vector3(min.x, min.y, min.z);
+			corners [1] = new Vector3(min.x, min.y, max.z);
+			corners [2] = new Vector3(max.x, min.y, min.z);
+			corners [3] = new Vector3(max.x, min.y, max.z);
+			int [] distances = new int [corners.Length];
+			bool solidground = true;
+
+			for(int I = 0; I < corners.Length; ++I)
 				{
-				// GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll; // TODO: Find bug which moves objects after placement // Question: Fixed?!
-				transform.position = new Vector3(transform.position.x, transform.position.y + 0.2f, transform.position.z);
+				print(I + ": " + corners[I]);
+				if(Physics.Raycast(corners[I], Vector3.down, out hit, 200))
+					{
+					distances[I] = Mathf.RoundToInt(hit.distance);
+					}
+				else
+					{
+					distances[I] = -1;
+					}
+				}
+
+			foreach(int distance in distances)
+				{
+				print(distance);
+				}
+
+			for(int I = 0; I < distances.Length; ++I)
+				{
+				if(distances[I] != distances[(I + 1) % distances.Length])
+					{
+					solidground = false;
+					}
+				}
+
+			//Snap to ground
+			if(Physics.Raycast(transform.position, Vector3.down, out hit, 200))
+				{
+				transform.position = hit.point;
+				}
+
+			// Check building requirements
+			if(Input.GetMouseButtonDown(0) && solidground && collidingStructures == 0)
+				{
+				transform.position = new Vector3(transform.position.x, transform.position.y - 0.8f, transform.position.z);
 				hasPlaced = true;
 				}
 			else
@@ -35,39 +80,17 @@ public class BuildableStructure : MonoBehaviour
 				}
 			}
 
-		if(collidingEnvironment < 0 || collidingStructures < 0)
+		/*if(collidingEnvironment < 0 || collidingStructures < 0)
 			{
 			print("Horrible explosion happening in BuildableStructure.cs!");
 			print("collidingEnvironment == " + collidingEnvironment);
 			print("collidingStructures == " + collidingStructures);
-			}
-		}
-
-	private void GroundApproximation()
-		{
-		if(collidingEnvironment == 0)
-			{
-			transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
-			}
-		if(collidingEnvironment > 0)
-			{
-			transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
-			}
-		startHeight = Mathf.RoundToInt(transform.position.y);
-		}
-
-	private bool checkTerrainConnection()
-		{
-		return true;
+			}*/
 		}
 
 	private void OnTriggerEnter(Collider other)
 		{
-		if(other.tag == environmentTag)
-			{
-			collidingEnvironment = 1;
-			}
-		else if(other.tag == structureTag)
+		if(other.tag == structureTag)
 			{
 			++collidingStructures;
 			}
@@ -75,13 +98,7 @@ public class BuildableStructure : MonoBehaviour
 
 	private void OnTriggerExit(Collider other)
 		{
-		if(other.tag == environmentTag)
-			{
-			print("call");
-			--collidingEnvironment;
-			print(collidingEnvironment);
-			}
-		else if(other.tag == structureTag)
+		if(other.tag == structureTag)
 			{
 			--collidingStructures;
 			}
